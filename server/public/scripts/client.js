500e30da4e9f5ff4b7a960991e980c688cb132c6
Matt Moua <matmoua@gmail.com>
2022-07-30 09:42:19 -0500

    Comments to show what's happening

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,55 +1,59 @@
 $(document).ready(onReady);
 
-let mode = '';
+let mode = ''; //This is where we store our mode/operator, we'll send this to the server so it knows what to do with our two numbers later.
 
 function onReady(){
-  $('#addBtn').on('click', modeChangeAddition);
-  $('#subtractBtn').on('click', modeChangeSubtraction);
-  $('#multiplyBtn').on('click', modeChangeMultiplication);
-  $('#divideBtn').on('click', modeChangeDivision);
-  $('#equalsBtn').on('click', runEquals);
+  $('#addBtn').on('click', modeChangeAddition); //When we click the + sign, change the mode
+  $('#subtractBtn').on('click', modeChangeSubtraction); // When we click the - sign...
+  $('#multiplyBtn').on('click', modeChangeMultiplication); // When we click the * sign...
+  $('#divideBtn').on('click', modeChangeDivision); // When we click the / sign...
+  $('#equalsBtn').on('click', runEquals); // When we click the = sign, send all info to server
 }
 
-function modeChangeAddition(){
-  $('#modeHere').empty();
+function modeChangeAddition(){ //When the + sign is clicked...
+  $('#modeHere').empty(); // Empty out the 'mode' spot on webpage and show the + sign
   $('#modeHere').append('+');
-  mode = '+';
+  mode = '+'; // Change the mode to be a string of the operator we'll use later at the server
 }
 
 function modeChangeSubtraction(){
-  $('#modeHere').empty();
+  $('#modeHere').empty(); // Empty out the 'mode' spot on webpage and show the - sign
   $('#modeHere').append('-');
-  mode = '-';
+  mode = '-'; // Change the mode to be a string of the operator we'll use later at the server
 }
 
 function modeChangeMultiplication(){
-  $('#modeHere').empty();
+  $('#modeHere').empty();// Empty out the 'mode' spot on webpage and show the * sign
   $('#modeHere').append('*');
-  mode = '*';
+  mode = '*'; // Change the mode to be a string of the operator we'll use later at the server
 }
 
 function modeChangeDivision(){
-  $('#modeHere').empty();
+  $('#modeHere').empty(); // Empty out the 'mode' spot on webpage and show the / sign
   $('#modeHere').append('/');
-  mode = '/';
+  mode = '/'; // Change the mode to be a string of the operator we'll use later at the server
 }
 
-function runEquals(){
+function runEquals(){ //This runs when we hit the equals button
   let input1 = $('#input1').val();
   let input2 = $('#input2').val();
-  if (mode === ''){
+  // We'll want to check to make sure both fields are filled and that a mode is selected, if not, throw an error
+  if (mode === ''){ // If the user hasn't selected a mode yet, throw error
     alert('Please select a mode!');
     return -1;
-  } else {
+  } else if ($('#input1').val() === '' || $('#input2').val() === ''){
+    // If either input text boxes are empty, throw an error
+    alert('Missing an input, please make sure both fields are filled');
+  } else { // This else will run if both fields are filled and the user has selected a mode.
     $.ajax({
       method: 'POST',
       url: '/compute',
-      data: {
+      data: { // Send and object to server containing both field values and the mode to use
           num1: input1,
           num2: input2,
           mode: mode
       }
-    }).then(function (){
+    }).then(function (){ // Clear out fields and mode, then reach out to server to compute
       $('#input1').val('');
       $('#input2').val('');
       $('#modeHere').empty();
@@ -59,14 +63,13 @@ function runEquals(){
   }
 }
 
-function getValue(){
+function getValue(){ // This function runs after data is sent to the server
   $.ajax({
     method: 'GET',
     url: '/compute'
-  }).then(function (response){
-    // console.log('answer is', response.key);
-    $('#resultsHere').empty();
-    $('#resultsHere').append(response.key);
+  }).then(function (response){ // Ajax should receive a response/object from server containing an object that contains our answer, and our string to append to the history Div
+    $('#resultsHere').empty(); // First, empty the results from previous equation.
+    $('#resultsHere').append(response.key); // Append the new answer to the results space as a new <li>
     $('#historyList').append(`
       <li>${response.history}</li>
     `);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// const inventoryList = require('./modules/inventoryList');
 
+// This will be our equation string which we'll use later once we get the right information
 equationString = [];
 
 //create instance of express!
@@ -9,20 +9,27 @@ const PORT = 5000;
 app.use(express.urlencoded({extended : true}));
 app.use(express.static('server/public'));
 
-app.get('/compute', (req, res) => {
+app.get('/compute', (req, res) => { // This is our get method that sends back the answer to the client after computing
+  // We'll create an object variable to store everything we'll need to send back, the answer and the string for the historyDiv.
   let answer = { 
+    // This is our history string to append as an <li> later
     history: equationString[0]+equationString[1]+equationString[2],
+    // They key is the result of the calculateThings function down below
+    // The calculateThings function will take our string from our POST method and compute it
     key: calculateThings(equationString) 
   }
-  res.send(answer);
+
+  res.send(answer); // Send the object back to client for use
 });
 
-app.post('/compute', (req, res) => {
+app.post('/compute', (req, res) => { // This is our post route, when information is sent to the server
+  // Empty the equation string first so we don't have old data
   equationString = [];
-  equationString.push(req.body.num1);
-  equationString.push(req.body.mode);
-  equationString.push(req.body.num2);
-  console.log(equationString);
+  // Our object sent to us from the client has 3 keys, input1, mode, and input 2, we'll push all 3 to the equationString array we declared at the top.
+  equationString.push(req.body.num1); // This will be equationString[0]
+  equationString.push(req.body.mode); // This will be equationString[1]
+  equationString.push(req.body.num2); // This will be equationString[2]
+  console.log(equationString); // Show the server console our completed equation to compute
   res.sendStatus(200); // 'OK'
 });
 
@@ -30,7 +37,9 @@ app.listen(PORT, function() {
   console.log('listening on port', PORT)
 });
 
-function calculateThings(){
+// This function will compute our string and give an answer to send back
+function calculateThings(){ 
+  // We'll check to see what mode we're in by checking equationString[1], then send accordingly.
   if (equationString[1] === '+'){
     return (Number(equationString[0]) + Number(equationString[2]));
   }
